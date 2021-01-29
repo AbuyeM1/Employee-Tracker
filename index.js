c0fd36bdcfffb274d4607404166465f8a87b080a
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-28 21:06:46 -0800

    add employee, assign their department

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 const inquirer = require('inquirer');
 
 
+
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -9,35 +10,91 @@ var connection = mysql.createConnection({
     database: "employeeDB"
 })
 
-connection.connect();
-Manager();
-function Manager(){
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'What do you want to do?',
-      name: 'namemanu',
-      choices:[ "Add departments","Add role", "Add employee"
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
+function start() {
+    inquirer
+        .prompt({
 
-      ],
-    },
-    // {
-    //   type: 'password',
-    //   message: 'What is your password?',
-    //   name: 'password',
-    // },
-    // {
-    //   type: 'password',
-    //   message: 'Re-enter password to confirm:',
-    //   name: 'confirm',
-    // },
-  ])
-  .then((response) =>{
-    console.log(response);
-    // response.confirm === response.password
-    //   ? console.log('Success!')
-    //   : console.log('You forgot your password already?!')
-    }
-  );
-}
+            type: 'list',
+            message: 'What do you want to do?',
+            name: 'Menu',
+            choices: ["view all employees",
+                "view all roles",
+                "view all department",
+                "Add departments",
+                "Add roles",
+                "Add employees"
+            ]
+        }
+
+        )
+        .then((answer) => {
+            switch (ans.choices) {
+                case "view all employees":
+                    viewEmployees();
+
+                    break;
+                case "view all roles":
+                    viewRols();
+                    break;
+                case "view all departments":
+                    viewDepartment();
+                    break;
+                case "update employee roles":
+                    updateRoles();
+                    break;
+                case "Add departments":
+                    addEmployee();
+                    break;
+                case "Add roles":
+                    addRoles();
+                    break;
+                case "Add employees":
+                    addDepartment();
+                    break;
+                case "exit":
+                    connection.end();
+                    break;
+
+            }
+        }
+        );
+};
+
+// function viewEmployees() {
+//     var query = "SELECT ";
+//     connection.query(query, function (err, res) {
+//         if (err) throw err;
+//         start();
+//     });
+// };
+// function viewRols() {
+//     var query = "SELECT ";
+//     connection.query(query, function (err, res) {
+//         if (err) throw err;
+//         start();
+//     });
+// };
+// function viewDepartment() {
+//     var query = "SELECT ";
+//     connection.query(query, function (err, res) {
+//         if (err) throw err;
+//         start();
+//     });
+// };
+// function updateRoles() {
+//     var query = "SELECT ";
+//     connection.query(query, function (err, res) {
+//         if (err) throw err;
+//         start();
+//     });
+// };
+
+
+
+
+
+
